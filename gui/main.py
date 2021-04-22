0b9016d8b835beffc58e3bcf9fb3102824703229
ggory15 <ggory15@snu.ac.kr>
2021-04-22 10:42:31 +0900

    initialized

--- gui/main.py
+++
@@ -1,9 +0,0 @@
-import wx
-from dwm1001 import DwmDeviceManager
-from LocationTrackerFrame import LocationTrackerFame
-
-if __name__ == '__main__':
-    app = wx.App()
-    device_manager = DwmDeviceManager()
-    frame = LocationTrackerFame(device_manager)
-    app.MainLoop()
